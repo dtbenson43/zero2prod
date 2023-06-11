ebead6d8422a35094cf86578aad23a790f3f199f
Daniel Benson <dbenson24@gmail.com>
2023-06-11 12:19:36 -0500

    clean up deps, create telemetry.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        std::io::stdout
-    );
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
@@ -31,6 +18,8 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
+
     let listener = TcpListener::bind(address)?;
+
     run(listener, connection_pool)?.await
 }
